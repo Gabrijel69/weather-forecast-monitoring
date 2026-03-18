8033764a7d31c0887bb93b56ebb35448e2955430
Gabrijel69 <119818187+Gabrijel69@users.noreply.github.com>
2026-03-18 10:00:48 +0100

    Inicijalni commit

---
+++ weather-mon/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="greetings">
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+.greetings h1 {
+  text-align: center;
+}
+
+</style>
