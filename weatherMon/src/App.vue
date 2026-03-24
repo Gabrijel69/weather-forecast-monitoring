ff9fd9f569cf6d6cbd85574c41acb2658207947d
DIN01234 <dino.ahmetovic05@gmail.com>
2026-03-24 11:39:20 +0100

    zamjenio google maps sa leaflet

--- weatherMon/src/App.vue
+++ weatherMon/src/App.vue
@@ -1,20 +1,30 @@
 <script setup>
-import { GoogleMap, Marker } from 'vue3-google-map'
+import { onMounted } from "vue";
+import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
 
-const center = { lat: 40.689247, lng: -74.044502 }
+onMounted(() => {
+  // inicijalizacija mape
+  const map = L.map("map").setView([45.815, 15.981], 5);
+
+  // OpenStreetMap layer
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenStreetMap contributors"
+  }).addTo(map);
+
+  // test marker (Zagreb)
+  L.marker([45.815, 15.981])
+    .addTo(map)
+    .bindPopup("Zagreb 🚢")
+    .openPopup();
+});
 </script>
 
 <template>
+
   <h1>Weather Monitoring for Ships</h1>
 
-  <GoogleMap
-  api-key="KEY"
-  style="width: 100%; height: 500px"
-  :center="center"
-  :zoom="15"
-  >
-    <Marker :options="{ position: center }" />
-  </GoogleMap>
+  <div id="map" style="height: 800px;"></div>
 
 </template>
 
